e31c307be47406c15f4f3395ba9f2dad586082cb
Milan Poznan <milan@jl.is>
2021-06-06 14:40:28 +0200

    add backend url

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `http://localhost:8888/smsale/graphql`,
+          `https://smsale.classyladyshop.com/graphql`,
       },
     },
     {
