f9d9bfbeb786eff1ec2fd09d0c3d01919fa789d8
Milan Poznan <milan@jl.is>
2021-06-23 09:53:35 +0200

    fx import capitall letter

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -8,7 +8,7 @@ import "@wordpress/block-library/build-style/style.css"
 import "@wordpress/block-library/build-style/theme.css"
 
 import Bio from "../components/bio"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
 const BlogPostTemplate = ({ data: { previous, next, post } }) => {
