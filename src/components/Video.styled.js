20f529d9b716f8c6fa9969827dcd6a6a72b22855
Milan Poznan <milan@jl.is>
2021-07-03 13:46:35 +0200

    add media query

--- src/components/Video.styled.js
+++ src/components/Video.styled.js
@@ -23,7 +23,7 @@ export const BackgroundVideoWrapper = styled.div`
   height: 100%;
   overflow: hidden;
 
-  @media ${device.laptop} {
+  @media (min-width: 768px) {
     max-height: 80vh;
     height: 80vh;
   }
@@ -35,7 +35,7 @@ export const VideoSection = styled.section`
   height: 50vh;
   
 
-  @media ${device.laptop} {
+  @media (min-width: 1024px) {
     max-height: 80vh;
     height: 80vh;
   }
